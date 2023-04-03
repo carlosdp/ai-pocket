969c57b4e59028fbed94e6a70f8bc3f3183c8c66
Carlos Diaz-Padron <carlos@metaphor.xyz>
2023-04-03 16:58:53 -0400

    add description

--- vite.ext.config.ts
+++ vite.ext.config.ts
@@ -7,6 +7,7 @@ const manifest = defineManifest({
   manifest_version: 3,
   name: 'Briefer',
   version: '1.0.0',
+  description: 'Save bookmarks and have a personal briefing the next morning',
   key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAn9kYEEcLOgnpxJ4lPXVNJr+GeUvcTODki1TXYmVs00Go05p2RsHjCeB9Y47MtBgT4gXqnMQVmjHd86fvaC8R/Ju7XMNifxL8Un6192UJ43ssvKwTeMI3y35O5/SYgPERvcxBnUtWRVN/EcJyExLKHmqWs09UxcaV/ayHB43utdq4FMV+G29JmYyVTQjkpPhoZRcy2l9UI2e+Wk9TX5GWDVkqtVkO6m5dySJl493V/WeBFppFqbkGid3lA/Oz9Chi1Xb8pIfIZjPbGkXhael6WPv3QpU/geJuQDMG4wz5GKTBAiM7dLwC6rzjSGzXTDzdQolP9KlG+S6w6U68BMQ4NQIDAQAB',
   action: {
     default_popup: 'src/extension/index.html',
