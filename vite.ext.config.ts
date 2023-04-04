6f3758dbad4abfc33a8b00a1b77f251ad16451c1
Carlos Diaz-Padron <carlos@metaphor.xyz>
2023-04-04 18:07:44 -0400

    add dev mode extension

--- vite.ext.config.ts
+++ vite.ext.config.ts
@@ -3,12 +3,18 @@ import { crx, defineManifest } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const DEV_KEY =
+  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4BBB8Xgl9I9XqhVy0RrNLuC0+zvKBo4WMylaeWVIsKDvaMWSGiZesqXl2kSBGvKp0pAByqGZopqa7OdhccyrBgzp1msxulD3GzWMF/gyEupAVyFRWHHGKY8AFR+M0GVHWo3ONEKxqYV6Nlvz8PzLH1BtKEsDJ0LC6gxm41OlYcZEXUOJW7DAT90GRQRDFVb2eWMcXCUF3qUSleVL00nnIecDPrgF+m/fOrab86I2gNL+NRtPO+XBmLRnUVgqV/HvasEEsTbMSRclQsaRChjdZvlbulOEyGQcE0/DjYg+39Kq/cU84Eyndg10pvmUkLR+54UeoZn1obVCWHpbAfxTQQIDAQAB';
+const PROD_KEY =
+  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAn9kYEEcLOgnpxJ4lPXVNJr+GeUvcTODki1TXYmVs00Go05p2RsHjCeB9Y47MtBgT4gXqnMQVmjHd86fvaC8R/Ju7XMNifxL8Un6192UJ43ssvKwTeMI3y35O5/SYgPERvcxBnUtWRVN/EcJyExLKHmqWs09UxcaV/ayHB43utdq4FMV+G29JmYyVTQjkpPhoZRcy2l9UI2e+Wk9TX5GWDVkqtVkO6m5dySJl493V/WeBFppFqbkGid3lA/Oz9Chi1Xb8pIfIZjPbGkXhael6WPv3QpU/geJuQDMG4wz5GKTBAiM7dLwC6rzjSGzXTDzdQolP9KlG+S6w6U68BMQ4NQIDAQAB';
+const NAME = 'Briefer';
+
 const manifest = defineManifest({
   manifest_version: 3,
-  name: 'Briefer',
+  name: `${NAME}${process.env.NODE_ENV === 'production' ? '' : ' Dev'}`,
   version: '1.0.0',
   description: 'Save bookmarks and have a personal briefing the next morning',
-  key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAn9kYEEcLOgnpxJ4lPXVNJr+GeUvcTODki1TXYmVs00Go05p2RsHjCeB9Y47MtBgT4gXqnMQVmjHd86fvaC8R/Ju7XMNifxL8Un6192UJ43ssvKwTeMI3y35O5/SYgPERvcxBnUtWRVN/EcJyExLKHmqWs09UxcaV/ayHB43utdq4FMV+G29JmYyVTQjkpPhoZRcy2l9UI2e+Wk9TX5GWDVkqtVkO6m5dySJl493V/WeBFppFqbkGid3lA/Oz9Chi1Xb8pIfIZjPbGkXhael6WPv3QpU/geJuQDMG4wz5GKTBAiM7dLwC6rzjSGzXTDzdQolP9KlG+S6w6U68BMQ4NQIDAQAB',
+  key: process.env.NODE_ENV === 'production' ? PROD_KEY : DEV_KEY,
   icons: {
     16: 'public/favicon-16x16.png',
     32: 'public/favicon-32x32.png',
