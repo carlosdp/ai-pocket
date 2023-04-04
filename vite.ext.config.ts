b768d874cbf89b4200807aa885fb5c07057451b7
Carlos Diaz-Padron <carlos@metaphor.xyz>
2023-04-04 12:22:59 -0400

    add favicon

--- vite.ext.config.ts
+++ vite.ext.config.ts
@@ -9,6 +9,12 @@ const manifest = defineManifest({
   version: '1.0.0',
   description: 'Save bookmarks and have a personal briefing the next morning',
   key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAn9kYEEcLOgnpxJ4lPXVNJr+GeUvcTODki1TXYmVs00Go05p2RsHjCeB9Y47MtBgT4gXqnMQVmjHd86fvaC8R/Ju7XMNifxL8Un6192UJ43ssvKwTeMI3y35O5/SYgPERvcxBnUtWRVN/EcJyExLKHmqWs09UxcaV/ayHB43utdq4FMV+G29JmYyVTQjkpPhoZRcy2l9UI2e+Wk9TX5GWDVkqtVkO6m5dySJl493V/WeBFppFqbkGid3lA/Oz9Chi1Xb8pIfIZjPbGkXhael6WPv3QpU/geJuQDMG4wz5GKTBAiM7dLwC6rzjSGzXTDzdQolP9KlG+S6w6U68BMQ4NQIDAQAB',
+  icons: {
+    16: 'public/favicon-16x16.png',
+    32: 'public/favicon-32x32.png',
+    48: 'public/favicon-48x48.png',
+    128: 'public/favicon-128x128.png',
+  },
   action: {
     default_popup: 'src/extension/index.html',
   },
