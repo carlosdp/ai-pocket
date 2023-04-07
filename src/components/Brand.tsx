6fa0c95f4bbf7de32dfab04d1d5202487feda42c
Carlos Diaz-Padron <carlos@metaphor.xyz>
2023-04-07 14:18:21 -0400

    change logo

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const Brand = () => {
   return (
     <LinkBox as={Flex} alignItems="center" gap="12px" fontSize="xl" fontWeight="bold">
-      <Image width="32px" height="32px" src="/favicon-32x32.png" />
+      <Image width="36px" height="36px" src="/pouchlogo-small.png" />
       <LinkOverlay as={Link} to="/">
         Pouch
       </LinkOverlay>
